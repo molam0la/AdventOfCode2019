4e0694fd36b76c7534861c0f909dd3d104a141a2
sandra.jarzebska@gmail.com <sandra.jarzebska@gmail.com>
2020-01-05 17:39:07 +0100

    Solution to part 2

--- src/main/java/Day2.java
+++ src/main/java/Day2.java
@@ -1,11 +1,8 @@
 import java.util.Arrays;
-import java.util.List;
 
 public class Day2 {
 
-    public int[] runOptcode(int[] intInput) {
-
-//        for (int value : intInput) {
+    public int[] runOpcode(int[] intInput) {
 
         for (int i = 0; i < intInput.length; i++) {
 
@@ -16,47 +13,19 @@ public class Day2 {
 
             switch (intInput[i]) {
                 case 1:
-                    System.out.println("Original array: " + Arrays.toString(intInput));
-                    int optcode1Index = i;
-                    System.out.println("optcode1Index " + optcode1Index);
-
-
-                    firstInput = intInput[optcode1Index + 1];
-                    System.out.println("firstInput " + firstInput);
-
-                    //problem is the helper method, giving wrong index if duplicate values are present
-
-                    secondInput = intInput[optcode1Index + 2];
-                    System.out.println("secondInput " + secondInput);
-
-                    output = intInput[optcode1Index + 3];
-
+                    firstInput = intInput[i + 1];
+                    secondInput = intInput[i + 2];
+                    output = intInput[i + 3];
                     result = intInput[firstInput] + intInput[secondInput];
-                    System.out.println("result of " + intInput[firstInput] + " + " + intInput[secondInput] + " = " + result + "\n");
-
                     intInput[output] = result;
-                    System.out.println("result Optcode1 " + Arrays.toString(intInput) + "\n");
                     i += 3;
                     break;
                 case 2:
-                    System.out.println("Original array: " + Arrays.toString(intInput));
-
-                    int optcode2Index = i;
-                    System.out.println("optcode2Index " + optcode2Index);
-
-
-                    firstInput = intInput[optcode2Index + 1];
-                    System.out.println("firstInput " + firstInput);
-
-                    secondInput = intInput[optcode2Index + 2];
-                    System.out.println("secondInput " + secondInput);
-
-                    output = intInput[optcode2Index + 3];
+                    firstInput = intInput[i + 1];
+                    secondInput = intInput[i + 2];
+                    output = intInput[i + 3];
                     result = intInput[firstInput] * intInput[secondInput];
-                    System.out.println("result of " + intInput[firstInput] + " * " + intInput[secondInput] + " = " + result + "\n");
-
                     intInput[output] = result;
-                    System.out.println("result Optcode2: " + Arrays.toString(intInput) + "\n");
                     i += 3;
                     break;
                 case 99:
@@ -66,13 +35,30 @@ public class Day2 {
         return intInput;
     }
 
+    public int[] findVerbAndNoun() {
+        int[] newArray = new int[150];
+        for (int x = 0; x <= 100; x++) {
+            for (int y = 0; y <= 100; y++) {
+                newArray = new int[]{1, x, y, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 1, 5, 19, 23, 1, 23, 5, 27, 1, 27, 13, 31, 1, 31, 5, 35, 1, 9, 35, 39, 2, 13, 39, 43, 1, 43, 10, 47, 1, 47, 13, 51, 2, 10, 51, 55, 1, 55, 5, 59, 1, 59, 5, 63, 1, 63, 13, 67, 1, 13, 67, 71, 1, 71, 10, 75, 1, 6, 75, 79, 1, 6, 79, 83, 2, 10, 83, 87, 1, 87, 5, 91, 1, 5, 91, 95, 2, 95, 10, 99, 1, 9, 99, 103, 1, 103, 13, 107, 2, 10, 107, 111, 2, 13, 111, 115, 1, 6, 115, 119, 1, 119, 10, 123, 2, 9, 123, 127, 2, 127, 9, 131, 1, 131, 10, 135, 1, 135, 2, 139, 1, 10, 139, 0, 99, 2, 0, 14, 0};
+                runOpcode(newArray);
+                if (newArray[0] == 19690720) {
+                    return newArray;
+                }
+            }
+        }
+        return newArray;
+    }
+
     public static void main(String[] args) {
         String input = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,5,19,23,1,23,5,27,1,27,13,31,1,31,5,35,1,9,35,39,2,13,39,43,1,43,10,47,1,47,13,51,2,10,51,55,1,55,5,59,1,59,5,63,1,63,13,67,1,13,67,71,1,71,10,75,1,6,75,79,1,6,79,83,2,10,83,87,1,87,5,91,1,5,91,95,2,95,10,99,1,9,99,103,1,103,13,107,2,10,107,111,2,13,111,115,1,6,115,119,1,119,10,123,2,9,123,127,2,127,9,131,1,131,10,135,1,135,2,139,1,10,139,0,99,2,0,14,0";
         FileLoader f = new FileLoader();
         int[] intInput = f.toIntArray(input, ",");
         Day2 d2 = new Day2();
-        int[] processedProgramme = d2.runOptcode(intInput);
+        int[] partA = d2.runOpcode(intInput);
+        int[] partB = d2.findVerbAndNoun();
 
-        System.out.println("Result " + processedProgramme[0]);
+        System.out.println("Result partA " + partA[0]);
+        System.out.println("Result partB " + partB[1] + "" + partB[2]);
+
     }
 }
--- src/test/java/Day2Test.java
+++ src/test/java/Day2Test.java
@@ -1,7 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import static junit.framework.TestCase.assertEquals;
@@ -29,21 +28,21 @@ public class Day2Test {
 
     @Test
     public void testOptcode1LargeSample() {
-        assertEquals(70, d2.runOptcode(intInput)[3]);
+        assertEquals(70, d2.runOpcode(intInput)[3]);
     }
 
     @Test
     public void testOptcode1SmallSample() {
         stringInput = "1,0,0,0,99";
         intInput = f.toIntArray(stringInput, ",");
-        assertEquals(2, d2.runOptcode(intInput)[0]);
+        assertEquals(2, d2.runOpcode(intInput)[0]);
     }
 
     @Test
     public void testOptcode2LargeSample() {
-        assertEquals(3500, d2.runOptcode(intInput)[0]);
+        assertEquals(3500, d2.runOpcode(intInput)[0]);
         int[] expectedResult = {3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50};
-        int[] actualResult = d2.runOptcode(intInput);
+        int[] actualResult = d2.runOpcode(intInput);
         assertTrue(Arrays.equals(expectedResult, actualResult));
     }
 
@@ -51,14 +50,14 @@ public class Day2Test {
     public void testOptcode2SmallSample() {
         stringInput = "2,3,0,3,99";
         intInput = f.toIntArray(stringInput, ",");
-        assertEquals(6, d2.runOptcode(intInput)[3]);
+        assertEquals(6, d2.runOpcode(intInput)[3]);
     }
 
     @Test
     public void testOptcode99SmallSample() {
         stringInput = "2,4,4,5,99,0";
         intInput = f.toIntArray(stringInput, ",");
-        assertEquals(9801, d2.runOptcode(intInput)[5]);
+        assertEquals(9801, d2.runOpcode(intInput)[5]);
     }
 
     @Test
@@ -66,7 +65,7 @@ public class Day2Test {
         stringInput = "1,1,1,4,99,5,6,0,99";
         intInput = f.toIntArray(stringInput, ",");
         int[] expectedResult = {30, 1, 1, 4, 2, 5, 6, 0, 99};
-        int[] actualResult = d2.runOptcode(intInput);
+        int[] actualResult = d2.runOpcode(intInput);
         System.out.println("actual result" + Arrays.toString(actualResult));
         System.out.println("expected result" + Arrays.toString(expectedResult));
 
@@ -76,14 +75,21 @@ public class Day2Test {
     @Test
     public void testAllOptcodesTogether() {
         int[] expectedResult = {3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50};
-        int[] actualResult = d2.runOptcode(intInput);
+        int[] actualResult = d2.runOpcode(intInput);
         assertTrue(Arrays.equals(expectedResult, actualResult));
     }
 
     @Test
     public void testAllOptcodesTogether2() {
         int[] expectedResult = {3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50};
-        int[] actualResult = d2.runOptcode(intInput);
+        int[] actualResult = d2.runOpcode(intInput);
         assertTrue(Arrays.equals(expectedResult, actualResult));
+    }
+
+    @Test
+    public void testFindingNounAndVerb() {
+        int expectedResult = 19690720;
+        int[] actualResult = d2.findVerbAndNoun();
+        assertEquals(expectedResult, actualResult[0]);
     }
 }
