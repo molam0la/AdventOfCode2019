97420ddea3f46cb85b52891bb8c87dc3b3921bd5
sandra.jarzebska@gmail.com <sandra.jarzebska@gmail.com>
2020-01-06 13:54:51 +0100

    Solution to Day3 part1

--- src/test/java/Day3Test.java
+++ src/test/java/Day3Test.java
@@ -1,36 +1,35 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Set;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class Day3Test {
 
     Day3 d3;
     String testWires1;
     String testWires2;
+    Set<Point> wire1Points;
+    Set<Point> wire2Points;
 
     @Before
     public void setUp() {
-
         d3 = new Day3();
-        testWires1 = "R8,U5,L5,D3";
-        testWires2 = "U7,R6,D4,L4";
     }
 
     @Test
-    public void testConvertingWireCodeIntoListOfStrings() {
-        Set<Point> wire1Points = d3.runWire(d3.convertWire(testWires1));
+    public void testCalculatingMinimumDistanceSample1() {
+        testWires1 = "R8,U5,L5,D3";
+        testWires2 = "U7,R6,D4,L4";
+        wire1Points = d3.runWire(d3.convertWire(testWires1));
         System.out.println(wire1Points);
-
-        Set<Point> wire2Points = d3.runWire(d3.convertWire(testWires2));
+        wire2Points = d3.runWire(d3.convertWire(testWires2));
         System.out.println(wire2Points);
 
-        Integer minimumDistance = wire1Points.stream()
-                .filter(wire2Points::contains) // Filter so we only have points which are both in wire1 and wire2 (intersections) - this uses the Point.equals method to check if points are the same
-                .map(point -> Math.abs(point.getX()) + Math.abs(point.getY())) // Map each point to the manhattan distance from the origin
-                .min(Integer::compareTo) // Get the minimum manhattan distance
-                .orElse(0);
-
-        System.out.println(minimumDistance);
+        int minimumDistance = d3.calculateManhattanDistance(wire1Points, wire2Points);
+        assertEquals(6, minimumDistance);
     }
+
 }
---
+++ src/test/java/ParametarisedDay3Test.java
@@ -0,0 +1,31 @@
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.experimental.categories.Category;
+//import org.junit.runner.RunWith;
+//import org.junit.runners.Parameterized;
+//
+//
+//import java.util.Arrays;
+//import java.util.Collection;
+//
+//import static org.junit.Assert.*;
+//
+//@RunWith(Parameterized.class)
+////@Category(UnitTest.class)
+//public class ParametarisedDay3Test {
+//
+//    @Parameterized.Parameters
+//    public static Collection<Object[]> data() {
+//        return Arrays.asList(new Object[][] {
+//                { 0, 0, 0 }, { 1, 1, 2 }
+//        });
+//    }
+//
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @Test
+//    public void calculateManhattanDistance() {
+//    }
+//}
